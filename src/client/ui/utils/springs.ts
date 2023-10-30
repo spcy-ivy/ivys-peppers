751687c452b75c361e57b651097293c12595928d
WorshipMeOrElse <sylviaviajung@gmail.com>
2023-10-30 13:58:43 -0700

    fix(env): oops! forgot to gitignore the env!!

--- src/client/ui/utils/springs.ts
+++ src/client/ui/utils/springs.ts
@@ -1,7 +1,9 @@
 import { config, SpringOptions } from "@rbxts/ripple";
 
-export const springs = {
+export const springs: { [config: string]: SpringOptions } = {
 	...config.spring,
 	bubbly: { tension: 300, friction: 20, mass: 1.2 },
 	responsive: { tension: 600, friction: 34, mass: 0.7 },
-} satisfies { [config: string]: SpringOptions };
+};
+// getting rid of satisfies since eslint/prettier doesnt like it lmao
+// satisfies { [config: string]: SpringOptions };
