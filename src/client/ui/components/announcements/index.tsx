b3371ab9a10b2fb8a929c41d9c637ad815dc8780
WorshipMeOrElse <sylviaviajung@gmail.com>
2023-11-18 11:55:52 -0800

    feat: added vc
    closes #8

--- src/client/ui/components/announcements/index.tsx
+++ src/client/ui/components/announcements/index.tsx
@@ -2,24 +2,27 @@ import Roact, { useEffect, useState } from "@rbxts/roact";
 import { Announcement } from "./announcement";
 import { Events } from "client/network";
 
+// make multiple announcements be able to be stacked at once
+// look up a youtube tutorial or something
+
 export function Announcements() {
-	const [text, setText] = useState("science!!!");
-	const [enabled, setEnabled] = useState(false);
+  const [text, setText] = useState("science!!!");
+  const [enabled, setEnabled] = useState(false);
 
-	useEffect(() => {
-		const connection = Events.announce.connect((text) => {
-			setText(text);
-			setEnabled(true);
-			task.wait(5);
-			setEnabled(false);
-		});
+  useEffect(() => {
+    const connection = Events.announce.connect((text) => {
+      setText(text);
+      setEnabled(true);
+      task.wait(5);
+      setEnabled(false);
+    });
 
-		return () => connection.Disconnect();
-	});
+    return () => connection.Disconnect();
+  });
 
-	return (
-		<screengui IgnoreGuiInset={true}>
-			<Announcement text={text} enabled={enabled} />
-		</screengui>
-	);
+  return (
+    <screengui IgnoreGuiInset={true}>
+      <Announcement text={text} enabled={enabled} />
+    </screengui>
+  );
 }
