80f428c3919cf75283b37a3cde7ef85ae74b7a58
WorshipMeOrElse <sylviaviajung@gmail.com>
2023-12-26 11:12:23 -0800

    feat: removed player collision
    i didnt even test this lets assume this works
    
    closes #26

---
+++ src/server/services/NoPlayerCollision.ts
@@ -0,0 +1,26 @@
+import { Players, PhysicsService } from "@rbxts/services";
+import { OnStart, Service } from "@flamework/core";
+
+const group = "Players";
+
+@Service()
+export class NoPlayerCollision implements OnStart {
+	onStart() {
+		PhysicsService.RegisterCollisionGroup(group);
+		PhysicsService.CollisionGroupSetCollidable(group, group, false);
+
+		// jesus christ lord up above thats a lot of nesting
+		Players.PlayerAdded.Connect((player) =>
+			player.CharacterAdded.Connect((character) => {
+				character.ChildAdded.Connect((child) => {
+					if (child.IsA("BasePart")) child.CollisionGroup = group;
+				});
+
+				// "wow why are you repeating yourself here" shut up imaginary person i made up in my head its literally only two uses
+				character.ChildAdded.Connect((child) => {
+					if (child.IsA("BasePart")) child.CollisionGroup = group;
+				});
+			}),
+		);
+	}
+}
