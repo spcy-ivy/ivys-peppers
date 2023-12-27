7d1ee988aea0b72e49a68c57331e2abdd8495948
WorshipMeOrElse <sylviaviajung@gmail.com>
2023-12-27 10:05:14 -0800

    fix: fixed player collision

--- src/server/services/NoPlayerCollision.ts
+++ src/server/services/NoPlayerCollision.ts
@@ -1,7 +1,7 @@
-import { Players, PhysicsService } from "@rbxts/services";
-import { OnStart, Service } from "@flamework/core";
+import { Service, OnStart } from "@flamework/core";
+import { PhysicsService, Players } from "@rbxts/services";
 
-const group = "Player";
+const group = "Participant";
 
 @Service()
 export class NoPlayerCollision implements OnStart {
@@ -9,18 +9,15 @@ export class NoPlayerCollision implements OnStart {
 		PhysicsService.RegisterCollisionGroup(group);
 		PhysicsService.CollisionGroupSetCollidable(group, group, false);
 
-		// jesus christ lord up above thats a lot of nesting
-		Players.PlayerAdded.Connect((player) =>
+		// every day i live in hell
+		Players.PlayerAdded.Connect((player) => {
 			player.CharacterAdded.Connect((character) => {
-				character.ChildAdded.Connect((child) => {
-					if (child.IsA("BasePart")) child.CollisionGroup = group;
+				character.GetChildren().forEach((child) => {
+					if (child.IsA("BasePart")) {
+						child.CollisionGroup = group;
+					}
 				});
-
-				// "wow why are you repeating yourself here" shut up imaginary person i made up in my head its literally only two uses
-				character.ChildAdded.Connect((child) => {
-					if (child.IsA("BasePart")) child.CollisionGroup = group;
-				});
-			}),
-		);
+			});
+		});
 	}
 }
